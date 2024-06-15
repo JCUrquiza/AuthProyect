195e12af3a833c0c9a7f8554a1172fcbf5b66f4d
Juan Carlos <gurquiza8@gmail.com>
2024-06-15 14:03:12 -0600

    Start implementation of guard

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 import Swal from 'sweetalert2';
@@ -13,6 +14,7 @@ export class LoginPageComponent {
 
   private fb = inject( FormBuilder );
   private authService = inject( AuthService );
+  private router = inject( Router );
 
   public myForm: FormGroup = this.fb.group({
     email: ['', [ Validators.required, Validators.email ]],
@@ -24,7 +26,7 @@ export class LoginPageComponent {
 
     this.authService.login( email, password )
       .subscribe({
-        next: () => console.log('Todo bien!!'),
+        next: () => this.router.navigateByUrl('/dashboard'),
         error: (error) => {
           Swal.fire('Error', error, 'error');
         }

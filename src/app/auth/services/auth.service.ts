5f655daa4a4331e7cc7ce58ec8d3ed4d2533775a
Juan Carlos <gurquiza8@gmail.com>
2024-06-15 15:24:05 -0600

    Finish function of logout

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -48,7 +48,10 @@ export class AuthService {
     const url = `${ this.baseUrl }/auth/check-token`;
     const token = localStorage.getItem('token');
 
-    if ( !token ) return of(false);
+    if ( !token ) {
+      this.logout();
+      return of(false);
+    }
 
     const headers = new HttpHeaders()
       .set('Authorization', `Bearer ${ token }`);
@@ -62,6 +65,13 @@ export class AuthService {
         })
       )
 
+  }
+
+
+  logout() {
+    localStorage.removeItem('token');
+    this._currentUser.set( null );
+    this._authStatus.set( AuthStatus.notAuthenticated );
   }
 
 

a193e842386f3344e781c25f1ac84ed0420e9014
Juan Carlos <gurquiza8@gmail.com>
2024-06-15 13:27:15 -0600

    Handle errors

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -21,8 +23,11 @@ export class LoginPageComponent {
     const { email, password } = this.myForm.value;
 
     this.authService.login( email, password )
-      .subscribe( success => {
-        console.log( success );
+      .subscribe({
+        next: () => console.log('Todo bien!!'),
+        error: (error) => {
+          Swal.fire('Error', error, 'error');
+        }
       });
 
   }
